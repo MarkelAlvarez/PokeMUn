5ac6a1b71a3f23ead51781daf9952105dd756ef9
Markel <bymarkel23@gmail.com>
2018-12-20 08:19:59 +0100

    Proba

--- PokeMUn/main.c
+++ PokeMUn/main.c
@@ -36,17 +36,17 @@ int main(int argc, char* args[])
 			screenSurface = SDL_GetWindowSurface(window);
 			//Fill the surface white
 			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+			//Load BMP image
+			HelloWorld = SDL_LoadBMP(".\\IMG\\PokeMUn.bmp");
+			if (HelloWorld == NULL)
+			{
+				printf("Unable to load image %s! SDL Error: %s\n", ".\\IMG\\PokeMUn.bmp", SDL_GetError());
+				//Si da error que salga de la aplicaion
+			}
 			//Update the surface
 			SDL_UpdateWindowSurface(window);
 			//Wait two seconds
 			SDL_Delay(2000);
-			//Load BMP image
-			HelloWorld = SDL_LoadBMP( "IMG/PokeMUn.bmp" );
-		    if(HelloWorld == NULL)
-		    {
-		        printf( "Unable to load image %s! SDL Error: %s\n", "IMG/PokeMUn.bmp", SDL_GetError() );
-		        //Si da error que salga de la aplicaion
-		    }
 		}
 	}
 
